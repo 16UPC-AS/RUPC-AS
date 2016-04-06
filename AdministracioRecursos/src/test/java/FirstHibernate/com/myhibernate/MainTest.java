72a4c51da3460744ec146a44096c41bb62e43287
Joan <->
2016-04-06 20:26:55 +0200

    Canvi prova

--- AdministracioRecursos/src/test/java/FirstHibernate/com/myhibernate/MainTest.java
+++ AdministracioRecursos/src/test/java/FirstHibernate/com/myhibernate/MainTest.java
@@ -11,11 +11,11 @@ public class MainTest {
 		// System.out.println(rec.getId()+" "+rec.getNom());
 		// }
 		Usuari u2 = new Usuari();
-		u2.setUsername("jj");
+		u2.setUsername("FakeUser");
 
 		u2 = UsuariRepo.getByPK(u2.getUniqueConstraint());
-		u2.setNom("Joan");
-		u2.setMail("bbb@bbb.as");
+		u2.setNom("FakeName");
+		u2.setMail("Fake@Mail.com");
 		UsuariRepo.saveOrUpdate(u2);
 
 		System.out.println(u2.getNom() + " " + u2.getUsername() + " " + u2.getMail());
