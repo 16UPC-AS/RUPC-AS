df008fb872313a0955da54918939db01920ae200
jpagesbri <->
2016-04-12 11:49:50 +0200

    refactor

--- AdministracioRecursos/src/test/java/testHibernate/TestOrdinadors.java
+++ AdministracioRecursos/src/test/java/testHibernate/TestOrdinadors.java
@@ -10,11 +10,11 @@ import repositories.RecursRepo;
 public class TestOrdinadors {
 
 	public static void main(String[] args) {
-		Recurs rec = new Recurs("FakePC");
-		rec = RecursRepo.getByPK(rec.getUniqueConstraint());
-		Objecte obj = ObjecteRepo.getByID(rec.getId());
-		Ordinador ord = new Ordinador(obj,"FakeMarca","FakeModel");
+		Recurs recOrd = new Recurs("FakePC", 1);
+		RecursRepo.saveOrUpdate(recOrd);
+		Objecte objOrd = new Objecte(recOrd, null, 0);
+		ObjecteRepo.saveOrUpdate(objOrd);
+		Ordinador ord = new Ordinador(objOrd,"FakeMarca","FakeModel");
 		OrdinadorRepo.saveOrUpdate(ord);
-
 	}
 }
--- AdministracioRecursos/src/test/java/testHibernate/TestProjectors.java
+++ AdministracioRecursos/src/test/java/testHibernate/TestProjectors.java
@@ -10,10 +10,13 @@ import repositories.RecursRepo;
 public class TestProjectors {
 
 	public static void main(String[] args) {
-		Recurs rec = new Recurs("FakeProjector");
-		rec = RecursRepo.getByPK(rec.getUniqueConstraint());
-		Objecte obj = ObjecteRepo.getByID(rec.getId());
-		Projector proj = new Projector(obj, "1234x5678");
+		
+		Recurs recProj = new Recurs("FakeProjector", 1);
+		RecursRepo.saveOrUpdate(recProj);
+		Objecte objProj = new Objecte(recProj, null, 1);
+		ObjecteRepo.saveOrUpdate(objProj);
+		
+		Projector proj = new Projector(objProj, "1234x5678");
 		ProjectorRepo.saveOrUpdate(proj);
 	}
 }
